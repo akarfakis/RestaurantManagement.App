d21364614835f0e8db5a3282ec067238ad739133
Tasos Karfakis <tasos.karfakis@cloudf.in>
2024-12-15 14:53:07 +0200

    customer repo,endpoints,service

---
+++ RestaurantManagement.Services/Services/Registrators/CustomerServicesRegistrator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantManagement.Db.Repositories.Customers;
+using RestaurantManagement.Services.Services.Customers;
+
+namespace RestaurantManagement.Services.Services.Registrators;
+
+public static class CustomerServicesRegistrator
+{
+    public static void AddCustomerServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<ICustomerService, CustomerService>();
+    }
+}
